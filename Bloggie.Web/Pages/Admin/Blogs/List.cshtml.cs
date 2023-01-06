28def487398224dbccd1e6c7582927deca443585
HP <HP@DESKTOP-PQ9AC9M>
2023-01-07 00:19:51 +0300

    list

--- Bloggie.Web/Pages/Admin/Blogs/List.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/List.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,17 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 {
     public class ListModel : PageModel
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public List<BlogPost> BlogPosts { get; set; }
+
+        public ListModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
         public void OnGet()
         {
+            BlogPosts =bloggieDbContext.BlogPosts.ToList();
         }
     }
 }
